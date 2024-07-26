2d0df1b5407500ebbf0f043908719d5c8a5cd9c7
Teresa Lavoie <teresa.lavoie@ucalgary.ca>
2024-07-25 18:01:15 -0600

    trying to publishDir with flask file instead of render.yaml file

--- casset/model/app.py
+++ casset/model/app.py
@@ -1,12 +1,16 @@
 ## Blueprint py file to run all db py files in parallel
 
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from users import users_bp
 from playlist import playlist_bp
 from comments import comments_bp
 from songs import songs_bp
 from friends import friends_bp
+import os
+
+# Define the path to the build directory
+BUILD_DIR = os.path.join(os.path.dirname(__file__), '../build')
 
 app = Flask(__name__)
 CORS(app)
@@ -17,6 +21,16 @@ app.register_blueprint(playlist_bp, url_prefix='/playlist')
 app.register_blueprint(comments_bp, url_prefix='/comments')
 app.register_blueprint(songs_bp, url_prefix = '/songs')
 app.register_blueprint(friends_bp, url_prefix = '/friends')
+
+# Serve the front-end's main file (index.html)
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+# Serve other static files
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory(app.static_folder, path)
 
 # if __name__ == '__main__':
 #     app.run(debug=True, port=5000)
